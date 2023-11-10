9f0b3f94c87db8921b214d37e3e66df1db1786a5
Shaki hossain <akm.shakib@gmail.com>
2023-11-10 19:31:17 +0600

    register as a method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use Exception;
 class Handler extends ExceptionHandler
 {
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use App\PaymentMethods\CashPayment;
 use App\PaymentMethods\PaymentMethodInterface;
-use App\PaymentMethods\PaypalPayment;
-use App\PaymentMethods\StripePayment;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,20 +12,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(PaymentMethodInterface::class, function ($app) {
-            // Get the desired payment method from configuration
-            $paymentMethod = config('payment.default_method');
 
-            // Return the appropriate implementation based on the configuration
-            switch ($paymentMethod) {
-                case 'paypal':
-                    return new PaypalPayment();
-                case 'stripe':
-                    return new StripePayment();
-                default:
-                    return new CashPayment();
-            }
-        });
+        $this->registerPaymentMethods();
     }
 
     /**
@@ -37,5 +22,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    protected function registerPaymentMethods(): void
+    {
+        $defaultMethod = config('payment.default_method');
+        $methods = config('payment.methods', []);
+
+        $this->app->bind(PaymentMethodInterface::class, function ($app) use ($defaultMethod, $methods) {
+            $selectedMethod = $methods[$defaultMethod] ?? $methods['cash'];
+
+            return $app->make($selectedMethod);
+        });
     }
 }
