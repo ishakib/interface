f4ecf0461e8f20e637832b81fd92a674ecde4072
Shaki hossain <akm.shakib@gmail.com>
2023-11-10 21:46:59 +0600

    code clean

--- app/PaymentMethods/CashPayment.php
+++ app/PaymentMethods/CashPayment.php
@@ -6,16 +6,12 @@ class CashPayment extends AbstractPayment
 {
     public function processPayment(float $amount): string
     {
-        // Additional logic specific to processing a cash payment
         $transactionId = $this->generateTransactionId();
 
-        // Simulate storing the payment information in a database
         $this->storePaymentInDatabase($transactionId, $amount);
 
-        // Log the payment details
         $this->logPayment($transactionId, $amount);
 
-        // Update order status or perform other order-related tasks
         $this->updateOrderStatus();
 
         return "Cash payment processed successfully. Transaction ID: $transactionId";
@@ -23,30 +19,18 @@ class CashPayment extends AbstractPayment
 
     private function generateTransactionId(): string
     {
-        // Simulate generating a unique transaction ID
         return uniqid('cash_', true);
     }
 
     private function storePaymentInDatabase(string $transactionId, float $amount): void
     {
-        // Simulate storing payment information in the database
-        // You would typically use an ORM or query builder for database operations
-        // For example, Eloquent ORM in Laravel
-        // Payment::create(['transaction_id' => $transactionId, 'amount' => $amount]);
     }
 
     private function logPayment(string $transactionId, float $amount): void
     {
-        // Simulate logging payment details
-        // You might use Laravel's logging functionality or a custom logging service
-        // Log::info("Cash payment processed - Transaction ID: $transactionId, Amount: $amount");
     }
 
     private function updateOrderStatus(): void
     {
-        // Simulate updating order status
-        // This could involve marking the order as paid or updating inventory
-        // You would typically interact with your order management system or ORM
-        // Order::where('id', $orderId)->update(['status' => 'paid']);
     }
 }
--- app/PaymentMethods/StripePayment.php
+++ app/PaymentMethods/StripePayment.php
@@ -8,51 +8,38 @@ class StripePayment extends AbstractPayment
 {
     public function processPayment(float $amount): string
     {
-        // Ensure the user is logged in
         $user = Auth::user();
         if (!$user) {
             return "User not authenticated. Unable to process Stripe payment.";
         }
 
         try {
-            // Use Laravel Cashier to create a Stripe charge
-            $user->charge($amount * 100, 'stripe'); // Amount is in cents, so multiply by 100
+            $user->charge($amount * 100, 'stripe');
 
-            // Additional logic specific to processing a Stripe payment
             $transactionId = $this->generateTransactionId();
 
-            // Log the payment details
             $this->logPayment($transactionId, $amount);
 
-            // Update order status or perform other order-related tasks
             $this->updateOrderStatus();
 
             return "Stripe payment processed successfully. Transaction ID: $transactionId";
         } catch (\Exception $e) {
-            // Handle any exceptions (e.g., insufficient funds, declined card, etc.)
+
             return "Stripe payment failed. Error: " . $e->getMessage();
         }
     }
 
     private function generateTransactionId(): string
     {
-        // Simulate generating a unique transaction ID
         return uniqid('stripe_', true);
     }
 
     private function logPayment(string $transactionId, float $amount): void
     {
-        // Simulate logging payment details
-        // You might use Laravel's built-in logging functionality or a custom logging service
-        // Log::info("Stripe payment processed - Transaction ID: $transactionId, Amount: $amount");
     }
 
     private function updateOrderStatus(): void
     {
-        // Simulate updating order status
-        // This could involve marking the order as paid or updating inventory
-        // You would typically interact with your order management system or ORM
-        // Order::where('id', $orderId)->update(['status' => 'paid']);
     }
 }
 
